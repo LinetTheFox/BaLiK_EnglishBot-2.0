4af91ec7f2af1431c54ef5a039506d4f7d5846d5
balikfromua <yuhimenko.valentin@gmail.com>
2019-02-16 20:56:20 +0200

    user dao done

--- src/main/java/balik/englishbot/database/User.java
+++ src/main/java/balik/englishbot/database/User.java
@@ -39,4 +39,28 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getUnit() {
+        return unit;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public int getCurrentQuestion() {
+        return currentQuestion;
+    }
+
+    public void setCurrentQuestion(int currentQuestion) {
+        this.currentQuestion = currentQuestion;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
--- src/main/java/balik/englishbot/database/UserDAO.java
+++ src/main/java/balik/englishbot/database/UserDAO.java
@@ -4,10 +4,8 @@ import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class UserDAO {
@@ -30,7 +28,11 @@ public class UserDAO {
             "score INT, " +
             "PRIMARY KEY (id) " +
             ")";
-    private static final String GET_BY_CHAT_ID = "SELECT * FROM users WHERE id = ?";
+    private static final String CREATE_USER = "INSERT INTO users (id, username, firstname, unit, current_question, score)" +
+            " VALUES (?,?,?,?,?,?)";
+    private static final String UPDATE_USER = "UPDATE users SET username=?, firstname=?, unit=?, current_question=?, score=?" +
+            " WHERE id=?";
+    private static final String GET_USER_BY_CHAT_ID = "SELECT * FROM users WHERE id = ?";
     private static final String GET_ALL_USERS = "SELECT * FROM users";
 
     private UserDAO() throws IOException {
@@ -58,7 +60,6 @@ public class UserDAO {
         return instance;
     }
 
-
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
     }
@@ -72,11 +73,9 @@ public class UserDAO {
         }
     }
 
-
     private void createTable() {
         try (Connection dbConnection = getConnection();
              Statement statement = dbConnection.createStatement()) {
-            // выполнить SQL запрос
             statement.execute(CREATE_TABLE);
             LOG.info("Table 'users' is created!");
         } catch (SQLException e) {
@@ -85,4 +84,112 @@ public class UserDAO {
         }
     }
 
+    public User getUser(long chatId) {
+        PreparedStatement statement;
+        ResultSet rs;
+
+        try (Connection con = getConnection()) {
+            statement = con.prepareStatement(GET_USER_BY_CHAT_ID);
+            statement.setString(1, String.valueOf(chatId));
+
+            rs = statement.executeQuery();
+
+            if (rs.next())
+                return extractUser(rs);
+
+        } catch (SQLException e) {
+            LOG.error("Can not get User" + e.getMessage());
+        }
+
+        return null;
+    }
+
+    public ArrayList<User> getAllUsers() {
+        PreparedStatement statement;
+        ResultSet rs;
+
+        ArrayList<User> users = new ArrayList<>();
+
+        try (Connection con = getConnection()) {
+            statement = con.prepareStatement(GET_ALL_USERS);
+
+            rs = statement.executeQuery();
+
+            while (rs.next())
+                users.add(extractUser(rs));
+
+        } catch (SQLException e) {
+            LOG.error("Can not get User" + e.getMessage());
+        }
+
+        return users;
+    }
+
+    private User extractUser(ResultSet rs) {
+        User user = null;
+
+        try {
+            user = new User(rs.getString("id"));
+            user.setName(rs.getString("firstname"));
+            user.setUsername(rs.getString("username"));
+            user.setUnit(Integer.parseInt(rs.getString("unit")));
+            user.setCurrentQuestion(Integer.parseInt(rs.getString("current_question")));
+            user.setScore(Integer.parseInt(rs.getString("score")));
+        } catch (SQLException e) {
+            LOG.error("Can not extract user\n" + e.getMessage());
+        }
+
+        return user;
+    }
+
+    public void updateUser(User user) {
+        PreparedStatement statement;
+
+        try (Connection con = getConnection()) {
+            statement = con.prepareStatement(UPDATE_USER);
+            int k = 1;
+            k = setUserData(k, user, statement);
+            statement.setString(k, String.valueOf(user.getChatId()));
+
+            int rs = statement.executeUpdate();
+            LOG.debug("User update result: " + rs);
+
+        } catch (SQLException e) {
+            LOG.error("Can not get User", e);
+        }
+    }
+
+    private int setUserData(int k, User user, PreparedStatement statement) throws SQLException {
+        statement.setString(k++, user.getUsername());
+        statement.setString(k++, user.getName());
+        statement.setInt(k++, user.getUnit());
+        statement.setInt(k++, user.getCurrentQuestion());
+        statement.setInt(k++, user.getScore());
+
+        return k;
+    }
+
+    public void createUser(User user) {
+        PreparedStatement statement;
+
+        if (getUser(user.getChatId()) != null) {
+            updateUser(user);
+            return;
+        }
+
+        try (Connection con = getConnection()) {
+
+
+            statement = con.prepareStatement(CREATE_USER);
+            int k = 1;
+            statement.setString(k++, String.valueOf(user.getChatId()));
+            setUserData(k, user, statement);
+
+            int rs = statement.executeUpdate();
+            LOG.debug("User create result: " + rs);
+
+        } catch (SQLException e) {
+            LOG.error("Can not create User\n" + e.getMessage());
+        }
+    }
 }
